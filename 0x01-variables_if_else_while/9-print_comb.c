b45c2eeb1b16ad524b64741625c73cfd53291247
shodyds <samuel.shodipo@outlook.com>
2023-12-15 17:40:33 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	char num = '0';
+	int num = 48;
 
-	while (num <= '9')
+	while (num <= 57)
 	{
 		putchar(num);
-		if (num == '9')
+		if (num == 57)
 			break;
 		putchar(',');
 		putchar(' ');
