16af0f14d8bd89365964c28091b21395ddfc051c
shodyds <samuel.shodipo@outlook.com>
2023-12-15 16:25:05 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Prints all single digit of base ten
+ * Return: 0
+ */
+int main(void)
+{
+	int num = 48;
+
+	while (num <= 57)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
+
+
