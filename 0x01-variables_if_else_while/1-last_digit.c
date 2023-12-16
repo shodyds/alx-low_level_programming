4b305da6904dac0d3e39a98995d27798639272a3
shodyds <samuel.shodipo@outlook.com>
2023-12-15 17:44:13 -0800

    1-last_digit.c update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,9 +23,7 @@ int main(void)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, o);
 	}
 	else
-	{	
 		printf("Last digit of %d is %d and is 0\n", n, o);
-	}
 	return (0);
 }
 
