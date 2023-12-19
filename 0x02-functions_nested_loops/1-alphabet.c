8e59da897e212ec1c218c31ce64912ea33f2b976
shodyds <samuel.shodipo@outlook.com>
2023-12-19 08:33:59 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: prints lowercase letters
+ */
+void print_alphabet(void)
+{
+	char c = 'a'
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar ('\n');
+}
