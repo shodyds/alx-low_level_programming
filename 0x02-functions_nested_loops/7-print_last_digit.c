dba3f64512085081353f411d6a3dcc0ebcc47647
shodyds <samuel.shodipo@outlook.com>
2023-12-19 13:25:36 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,16 +2,14 @@
 
 /**
  * print_last_digit - print and return the last value of num
- * @num - input character
+ * @num: input character
  * Return: last digit of num
  */
 int print_last_digit(int num)
 {
-	if (num < 0)
-	{
-		num = num * -1;
-	}
 	num = num % 10;
+	if (num < 0)
+		num = num * -1;
 	_putchar (num + '0');
 	return (num);
 }
