56d9953cbdf34ba2e672b738d2f7f4a563645df7
shodyds <samuel.shodipo@outlook.com>
2023-12-19 11:54:00 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - Function compute abosolute value
+ * @n: character to be computed
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+		return (n);
+	}
+	else
+		return (n);
+}
