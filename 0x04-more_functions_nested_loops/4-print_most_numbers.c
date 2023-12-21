a1477fda1f08a4dfa3c4fb35b85842f318ae0915
shodyds <samuel.shodipo@outlook.com>
2023-12-21 12:42:12 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Function print 1 to 9 except 2 and 4
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int c = '0';
+
+	while (c <= '9')
+	{
+		if (c != '2' && c != '4')
+		{
+			_putchar (c);
+			c++;
+		}
+	}
+}
