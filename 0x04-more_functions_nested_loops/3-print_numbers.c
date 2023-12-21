d085f2742fa4e7d63dd5027f97ca5c5e8cb20fe7
shodyds <samuel.shodipo@outlook.com>
2023-12-21 12:24:44 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - Function print 1 to 9
+ * Return: returns void
+ */
+void print_numbers(void)
+{
+	int c = '0';
+
+	while (c <= '9')
+	{
+		putchar (c);
+		c++;
+	}
+}
