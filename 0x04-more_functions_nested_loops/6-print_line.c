433259705ed1849c7b6b575d566af238ea8859d3
shodyds <samuel.shodipo@outlook.com>
2023-12-21 16:50:57 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,16 +8,18 @@
 
 void print_line(int n)
 {
-	int i, n;
+	int i;
 
-	if (n < 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (i = 0; i < n; i++)
+		for (i = 1; i < n; i++)
+		{
 			_putchar ('_');
+		}
 		putchar('\n');
 	}
 }
