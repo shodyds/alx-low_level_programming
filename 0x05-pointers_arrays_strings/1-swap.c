129965f5a9beb45abbb8ec7a1bc72fe3a434f199
shodyds <samuel.shodipo@outlook.com>
2024-01-02 16:08:40 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - Function swaps two numbers
+ * @a: pointer to the character to swap
+ * @b: pointer to the character to swap
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+	
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
