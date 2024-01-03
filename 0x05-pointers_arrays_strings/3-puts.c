21fa91249babf097b99215b62561e7b31ea2c028
shodyds <samuel.shodipo@outlook.com>
2024-01-02 16:48:17 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - Function print string
+ * @str: pointer to the address of the 1st element
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str = '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
