64e43236816fb8a54e5be436ba517d9d549cd20e
shodyds <samuel.shodipo@outlook.com>
2024-01-02 17:07:56 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "mian.h"
+
+/**
+ * print_rev - Function prints string in reverse
+ * @s: Pointer to the address of the first element
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	s--;
+	for (i = count; i > 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
