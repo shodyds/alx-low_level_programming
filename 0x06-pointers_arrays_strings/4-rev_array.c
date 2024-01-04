d9b5bd7d295439d1459a0ec46a6d4e5d51487e2e
shodyds <samuel.shodipo@outlook.com>
2024-01-04 14:29:19 -0800

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - print array in reverse
+ * @a: pointer to the first element of the array
+ * @n: number of element in the array
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int tmp, i = 0;
+
+	while (i < --n)
+	{
+		tmp = a[i];
+		a[i] = a[n];
+		a[n] = tmp;
+	}
+}
