28f6692cb40757423bc106e0416492cb650d7a71
shodyds <samuel.shodipo@outlook.com>
2024-01-04 13:04:07 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,12 +9,13 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0, j = 0;
+	int i = 0, j;
 
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
+	j = 0;
 	while (n > j && src[j] != '\0')
 	{
 		dest[i] = src[j];
