56c1bae5d09d7cd705268b38d72a26a8a87a1a47
shodyds <samuel.shodipo@outlook.com>
2024-01-04 13:24:05 -0800

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - Function copies string
+ * @dest: destination variable
+ * @src: source variable
+ * @n: number of characters to be copied
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (n > i && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (n > i)
+		dest[i] = '\0';
+	return (dest);
+}
