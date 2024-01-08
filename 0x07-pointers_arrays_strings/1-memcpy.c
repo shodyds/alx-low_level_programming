ae02e859fe1660309080516a79625ca617530937
shodyds <samuel.shodipo@outlook.com>
2024-01-08 12:02:12 -0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,15 +4,16 @@
  * _memcpy - Function copies memory area
  * @dest: pointer to the first element of memnory area
  * @src: character to be copied
+ * @n: number of character to be copied
  * Return: dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int i = 0;
 
-	while (n < 0 && src[i] != '\0')
+	while (n > 0 && src[i] != '\0')
 	{
 		dest[i] = src[i];
-		n++;
+		n--;
 	}
 }
