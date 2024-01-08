c0403527d6b597b3de30ec99e945715e44b06182
shodyds <samuel.shodipo@outlook.com>
2024-01-08 10:41:42 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * char *_memset - Function fills memory with constant byte
+ * @b: The character
+ * @s: pointer to the first element of address
+ * @n: number of character or byte to change
+ * Return: changed array or address via s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
