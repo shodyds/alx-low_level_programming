bdb645930965cb88ee529c80eeb16ec5a82d1316
shodyds <samuel.shodipo@outlook.com>
2024-01-15 17:16:46 -0800

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	alloc = malloc(height * size(int *));
+	alloc = (int **)malloc(height * sizeof(int *));
 	if (alloc == NULL)
 		return (NULL);
 	i = 0;
