0a4dbc962cd7bf5666fb46ff1c85f3ebdfc554aa
shodyds <samuel.shodipo@outlook.com>
2024-01-17 01:31:51 -0800

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * puts_recursion - prints a string
+ * @s: input string
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s+1);
+	}
+	_putchar('\n')
