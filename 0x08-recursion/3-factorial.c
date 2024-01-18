88bc3fa5deb17dde0d9db9f8cedea39873ec05b8
shodyds <samuel.shodipo@outlook.com>
2024-01-17 18:28:24 -0800

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * factorial - Function calaculates factorial of number
+ * @n: input character
+ * Return: factorial of number
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (1 * fact(n - 1));
+}
