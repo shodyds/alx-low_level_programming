438a7cc62eff4a2aa46f8abc8206116d8660f702
shodyds <samuel.shodipo@outlook.com>
2024-01-17 18:54:40 -0800

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,6 +4,7 @@
  * _pow_recursion - Function calculates x raised to power y
  * @x: input value
  * @y: input value
+ * Return: x raised to power y
  */
 int _pow_recursion(int x, int y)
 {
@@ -11,5 +12,5 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	if (y == 0)
 		return (1);
-	return (x * _pow_recursion(x, y));
+	return (x * _pow_recursion(x, (y - 1)));
 }
