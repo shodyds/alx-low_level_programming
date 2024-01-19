d9a275d6de240d6689c0ba1aa4733e3f3a838aa3
shodyds <samuel.shodipo@outlook.com>
2024-01-19 15:26:32 -0800

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,7 +10,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
-	unsigned int i, j, len1, len2;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
 	while (s1 && s1[len1])
 		len1++;
@@ -22,13 +22,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		str = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (!str)
 		return (NULL);
-	i = 0;
 	while (i < len1)
 	{
 		str[i] = s1[i];
 		i++;
 	}
-	j = 0;
 	while ((n < len2) && (i < (len1 + n)))
 	{
 		str[i] = s2[j];
