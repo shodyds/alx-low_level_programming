60fbff116091b8103a02d27af9fa576479fb8647
shodyds <samuel.shodipo@outlook.com>
2024-01-22 16:57:31 -0800

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,12 +1,12 @@
-#include "dog.h"
 #include <stdlib.h>
+#include "dog.h"
 
 /**
  * init_dog - Function initialize dog
+ * @d: address of struct dog var
  * @name: input value
  * @age: input value
  * @owner: dog owner
- * @d: address of struct dog var
  * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner);
