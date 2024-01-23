37429e726e1029f72b1cfbfd6dc934a422e783f1
shodyds <samuel.shodipo@outlook.com>
2024-01-22 16:47:28 -0800

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -4,13 +4,14 @@
  * init_dog - Function initialize dog
  * @name: input value
  * @age: input value
- * owner: dog owner
+ * @owner: dog owner
+ * @d: address of struct dog var
  * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 {
 	if (d == NULL)
-		d = malloc(sizeof(struct));
+		d = malloc(sizeof(struct dog));
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
